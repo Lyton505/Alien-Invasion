dbd3707c43851de7f61dacc4dd669b4ca7cfd24d
Lyton <65817176+Lyton505@users.noreply.github.com>
2023-09-07 18:28:53 -0500

    added ugly elf

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys, pygame
 from settings import Settings
 from ship import Ship
+from ugly_elf import UglyElf
 
 
 class AlienInvasion:
@@ -17,27 +18,36 @@ class AlienInvasion:
         pygame.display.set_caption("Incredible Alien Invasion Game")
         self.bg_color = (230, 230, 230)
         self.ship = Ship(self)
+        self.elf = UglyElf(self)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             # watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit(0)
+            self._check_events()
 
             # custom: 1 : try block to handle quitting game from keyboard
             try:
-                self.screen.fill(self.settings.bg_color)
-                self.ship.blitme()
-                # make the most recently drawn screen visible
-                pygame.display.flip()
+                self._update_screen()
             except Exception as exp:
                 print(f"\nException Stopped running")
                 sys.exit(-1)
             else:
                 self.clock.tick(60)
 
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+    def _update_screen(self):
+        """Update images on the screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        self.elf.blitme()
+        # make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make game instance and run game
